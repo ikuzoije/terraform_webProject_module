fea7d66d29fcb18a0d29d15ab2a3e47c39f18aaa
Ikenna Uzoije <ikennauzoije@MacBook-Pro.lan>
2022-09-25 18:24:31 -0400

    Added source in main.tf of use_module to pull the module from the github repository instead of local machine

--- use_module/main.tf
+++ use_module/main.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 module "proj_mod" { // This creates a webserver using the parameters defined in the modules/project folder
-  source = "../modules/webProject"
-  tags = { // Adding this tag attribute overrides the tag in the variable.tf file in the module
-    Owner       = "Ikenna Uzoije MODULE"
+  #source = "../modules/webProject" // Use this line if your module is in your local disk
+  source = "git@github.com:ikuzoije/terraform_webProject_module.git//modules/webProject" // Pulls from the github repository
+  tags = {
+    Owner       = "Ikenna Uzoije MODULE" // Adding this tag attribute overrides the tag in the variable.tf file in the module
     Environment = "Prod"
     Project     = "WebServer Bootstrap"
     Creator     = "Use_Module Resource"
